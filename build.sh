f2a651217a16ef96495467e836c26a11613ff59d
MrThanlon <1@ch34k.tk>
2019-05-28 20:18:20 +0800

    CI: fix build.sh error

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-apk install nodejs
+sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
+apk update
+apk add nodejs npm
 npm install --registry=https://registry.npm.taobao.org
 npm run build
 docker build -t $DOCKER_BUILD_TAG .
