529326dad6591788b5a0048f0492f30e49d49707
MrThanlon <1@ch34k.tk>
2019-05-28 21:49:54 +0800

    test

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
 apk update
 apk add nodejs npm
+node --version
+npm --version
 npm install --registry=https://registry.npm.taobao.org
 npm run build
 docker build -t $DOCKER_BUILD_TAG .
