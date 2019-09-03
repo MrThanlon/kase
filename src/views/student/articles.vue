4fff43804c7c6ad243795d93f7188d48eee0afc8
MrThanlon <i@anclx.cc>
2019-09-03 16:50:14 +0800

    add: /student
    Signed-off-by: MrThanlon <i@anclx.cc>

---
+++ src/views/student/articles.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        文章：{{cid}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "article",
+        props: {
+            cid: Number
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/student/subPannel.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        副面板
+    </div>
+</template>
+
+<script>
+    // 副面板
+    export default {
+        name: "sub"
+    }
+</script>
+
+<style scoped>
+
+</style>
