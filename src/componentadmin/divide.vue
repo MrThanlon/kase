bdef4719a1ccee87d7ba512af3d521531e7e93b8
田湘龙 <727393818@qq.com>
2019-09-07 14:00:55 +0800

    页面框架完成

--- src/componentadmin/divide.vue
+++ src/componentadmin/divide.vue
@@ -21,6 +21,7 @@
                              width="180">
               <template slot-scope="scope">
                 <el-button size="mini"
+                           type="danger"
                            @click="handleEdit(scope.$index, scope.row)">删除</el-button>
               </template></el-table-column>
           </el-table>
