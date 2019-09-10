1395c644354ca71a632d03b35accbe4de9ceccad
田湘龙 <727393818@qq.com>
2019-09-10 17:26:32 +0800

    增加 代理

--- src/main.js
+++ src/main.js
@@ -9,9 +9,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import store from '@/store'
 
-import api from '@/service/api'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import './axios'
+
+// import api from '@/service/api'
 
 import Vuex from 'vuex'
+
+Vue.use(VueAxios, axios)
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,16 @@
-const path = require('path');
+const path = require('path')
 
-function resolve(dir) {
-    return path.join(__dirname, dir)
+function resolve (dir) {
+  return path.join(__dirname, dir)
 }
 
 module.exports = {
-    publicPath: './',
-    chainWebpack: (config) => {
-        config.resolve.alias
-            .set('@$', resolve('src'))
-    }
-
+  publicPath: './',
+  devServer: {
+    proxy: 'http://kase.stuhome.com/'
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@$', resolve('src'))
+  }
 }
